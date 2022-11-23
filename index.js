516cbfa58e8bedc0fb62725199dc2c5970d58819
martin aguer <martin.aguer@bosonic.digital>
2022-11-23 14:55:02 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require("morgan");
+const cors = require("cors");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = 3000;
+const HOST = "localhost";
+const API_SERVICE_URL = "https://sandbox.plaid.com";
+
+app.use(cors({
+  origin: '*' // that will for all like  https / http 
+}))
+
+app.use(morgan('dev'));
+
+app.use('/plaid-link', createProxyMiddleware({
+   target: API_SERVICE_URL,
+   changeOrigin: true,
+   pathRewrite: {
+       [`^/plaid-link`]: '',
+   },
+   onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+
+app.listen(PORT, HOST, () => {
+   console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
